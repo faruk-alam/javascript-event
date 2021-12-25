002d965a59f55d44e04cde14823580e06702d26b
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-12-25 18:14:57 +0100

    Add files via upload

---
+++ onclick event/main.js
@@ -0,0 +1,16 @@
+function mymass(){
+    alert("Be Carefull");
+}
+function msg(){
+    var mssg = document.querySelector("#msg");
+    mssg.innerHTML = "you have a new message."
+}
+
+var picture = document.querySelector("#imgid");
+function skypic(){
+  picture.src = "images/sky.jpg";
+}
+function riverpic(){
+    picture.src = "images/river.jpg";
+}
+  
