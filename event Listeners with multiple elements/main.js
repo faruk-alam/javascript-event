c958e5939004a4c5f45d8563823ab02ec3419da9
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2022-01-05 13:48:17 +0100

    Add files via upload

---
+++ event Listeners with multiple elements/main.js
@@ -0,0 +1,33 @@
+// var myText = document.getElementById('clickText');
+// var btn = document.querySelectorAll('button')[0];
+// btn.addEventListener('click', function() {
+//    myText.innerHTML = "You have clicked button-1";
+// })
+
+// var myText = document.getElementById('clickText');
+// var btn = document.querySelectorAll('button')[1];
+// btn.addEventListener('click', function() {
+//    myText.innerHTML = "You have clicked button-2";
+// })
+
+// var myText = document.getElementById('clickText');
+// var btn = document.querySelectorAll('button')[2];
+// btn.addEventListener('click', function() {
+//    myText.innerHTML = "You have clicked button-3";
+// })
+
+// var myText = document.getElementById('clickText');
+// var btn = document.querySelectorAll('button')[0];
+// btn.addEventListener('click', function() {
+//    var text = this.innerHTML;
+//    myText.innerHTML = "You have clicked " + text;
+// })
+// Using loop....
+ for (var i = 0;i<3;i++){
+   var myText = document.getElementById('clickText');
+   var btn = document.querySelectorAll(".button")[i];
+   btn.addEventListener('click', function() {
+      var text = this.innerHTML;
+      myText.innerHTML = "You have clicked " + text;
+   })
+ }
